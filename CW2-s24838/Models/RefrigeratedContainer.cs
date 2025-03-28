65f333648e083e9bda436109e591940fcdf86195
s24838 <165217683+NataliaTrac@users.noreply.github.com>
2025-03-28 01:30:13 +0100

    Update required temperatures for product types in RefrigeratedContainer

--- CW2-s24838/Models/RefrigeratedContainer.cs
+++ CW2-s24838/Models/RefrigeratedContainer.cs
@@ -10,9 +10,16 @@ public class RefrigeratedContainer : Container, IHazardNotifer
 
     private static readonly Dictionary<ProductType, double> RequiredTemperatures = new()
     {
-        { ProductType.Banana, 13 },
-        { ProductType.Milk, 4 },
-        { ProductType.Hellium, -269 }
+        { ProductType.Bananas, 13.3},
+        { ProductType.Chocolate, 18 },
+        { ProductType.Fish, 2 },
+        { ProductType.Meat, -15 },
+        { ProductType.IceCream, -18 },
+        { ProductType.FrozenPizza, -30 },
+        { ProductType.Cheese, 7.2 },
+        { ProductType.Sausages, 5 },
+        { ProductType.Butter, 20.5 },
+        { ProductType.Eggs, 19 }
     };
 
     public RefrigeratedContainer(double tareWeight, double height, double depth, double maxLoadWeight,
